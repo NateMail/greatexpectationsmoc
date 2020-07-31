819dfb3eff724ca435a0bf071cbb5b5c7ddf2356
nathanmailhiot <nathanjm12@outlook.com>
2020-07-31 17:12:27 -0400

    Nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import Middle from "./components/Middle";
 import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
       <Middle />
       <Footer />
     </div>
---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <li>
+          <a href="#footer">Home</a>
+        </li>
+        <li>
+          <a href="#footer">About Us</a>
+        </li>
+        <li>
+          <a href="#footer">Pricing</a>
+        </li>
+        <li>
+          <a href="#footer">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
