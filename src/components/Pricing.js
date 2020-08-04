48a391cf33320ebd3022a5f5c16e666a99f1f158
nathanmailhiot <nathanjm12@outlook.com>
2020-08-04 18:23:07 -0400

    Pricing form start

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Pricing = () => {
+  const [value, setValue] = useState("");
+
+  function handleChange(event) {
+    event.preventDefault();
+    setValue(event);
+  }
+
   return (
     <div className="pricing" id="pricing">
       <h1 className="pricing__heading">Pricing</h1>
+
+      <form>
+        <label>
+          Name:
+          <input type="text" value={value} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
