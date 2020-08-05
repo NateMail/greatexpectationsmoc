d248df0294879c5c4dd99aaf6f2915cca8c87d3b
nathanmailhiot <nathanjm12@outlook.com>
2020-08-05 12:42:51 -0400

    All content on pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,11 @@ const Contact = () => {
   return (
     <div className="contact" id="contact">
       <h1 className="contact__heading">Contact</h1>
+      <p>
+        	For rates and scheduling, contact our business manager, Gabe Parker,
+ at: _________________________ 
+
+      </p>
     </div>
   );
 };
--- src/components/Rates.js
+++ src/components/Rates.js
@@ -4,6 +4,9 @@ const Rates = () => {
   return (
     <div className="rates" id="rates">
       <h1 className="rates__heading">Rates</h1>
+      <p>
+        Our rates vary depending on the length and content of the sessions, and the number of students.  Family discounts available for multiple students.  Please call to discuss rates and scheduling a complimentary first session.
+      </p>
     </div>
   );
 };
