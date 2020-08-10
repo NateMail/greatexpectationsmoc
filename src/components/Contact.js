dc25e6ae879eddf589470ba50ab30c07f583e611
nathanmailhiot <nathanjm12@outlook.com>
2020-08-10 12:06:58 -0400

    Contact form fields style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,8 +18,8 @@ export default function Contact() {
   }
 
   return (
-    <div className="contact">
-      <div className="contact__form__container">
+    <div className="contact" id="contact">
+      <div className="contact__container">
         <h2 className="contact__form__heading">Contact Us</h2>
         <form className="contact__form" onSubmit={sendEmail}>
           <input type="hidden" name="contact_number" />
@@ -27,23 +27,23 @@ export default function Contact() {
           <input
             type="text"
             name="user_name"
-            placeholder="name"
+            placeholder="Name"
             className="contact__form__input"
           />
           <label className="contact__form__label">Email</label>
           <input
             type="email"
             name="user_email"
-            placeholder="email"
+            placeholder="Email"
             className="contact__form__input"
           />
           <label className="contact__form__label">Message</label>
           <textarea
             name="message"
-            placeholder="message"
+            placeholder="Message"
             className="contact__form__input"
           />
-          <input type="submit" value="Send" />
+          <input className="contact__form__button" type="submit" value="Send" />
         </form>
       </div>
     </div>
