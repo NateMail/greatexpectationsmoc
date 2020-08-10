5c1c395d5add118563f0ea30559ab63ef939058e
nathanmailhiot <nathanjm12@outlook.com>
2020-08-10 11:19:38 -0400

    Basic contact form setup

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,51 @@
 import React from "react";
+// import emailjs from "emailjs-com";
 
-const Contact = () => {
+export default function Contact() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    // emailjs
+    //   .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+    //   .then(
+    //     (result) => {
+    //       console.log(result.text);
+    //     },
+    //     (error) => {
+    //       console.log(error.text);
+    //     }
+    //   );
+  }
+
   return (
-    <div className="contact" id="contact">
-      <h1 className="contact__heading">Contact</h1>
-      <p>
-        	For rates and scheduling, contact our business manager, Gabe Parker,
- at: _________________________ 
-
-      </p>
+    <div className="contact">
+      <div className="contact__form__container">
+        <h2 className="contact__form__heading">Contact Us</h2>
+        <form className="contact__form" onSubmit={sendEmail}>
+          <input type="hidden" name="contact_number" />
+          <label className="contact__form__label">Name</label>
+          <input
+            type="text"
+            name="user_name"
+            placeholder="name"
+            className="contact__form__input"
+          />
+          <label className="contact__form__label">Email</label>
+          <input
+            type="email"
+            name="user_email"
+            placeholder="email"
+            className="contact__form__input"
+          />
+          <label className="contact__form__label">Message</label>
+          <textarea
+            name="message"
+            placeholder="message"
+            className="contact__form__input"
+          />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
     </div>
   );
-};
-
-export default Contact;
+}
