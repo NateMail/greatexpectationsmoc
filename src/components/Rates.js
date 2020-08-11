53b98471b44eefecc47e5e8b95a6f59e955a382d
nathanmailhiot <nathanjm12@outlook.com>
2020-08-11 13:52:06 -0400

    Rates styling and font sizing

--- src/components/Rates.js
+++ src/components/Rates.js
@@ -3,10 +3,15 @@ import React from "react";
 const Rates = () => {
   return (
     <div className="rates" id="rates">
-      <h1 className="rates__heading">Rates</h1>
-      <p>
-        Our rates vary depending on the length and content of the sessions, and the number of students.  Family discounts available for multiple students.  Please call to discuss rates and scheduling a complimentary first session.
-      </p>
+      <h2 className="rates__heading">Rates</h2>
+      <div className="rates__container">
+        <p className="rates__message">
+          Our rates vary depending on the length and content of the sessions,
+          and the number of students. Family discounts available for multiple
+          students. Please call to discuss rates and scheduling a complimentary
+          first session.
+        </p>
+      </div>
     </div>
   );
 };
